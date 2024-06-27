15246d81b3c3993e2569356ffb5e79da9a8262ed
Diego Castillo <Diegoaz279@gmail.com>
2024-06-27 16:52:24 -0400

    Add project files.

---
+++ dowhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+class Program
+{
+    static void Main()
+    {
+        string nombre, apellido;
+        bool continuar;
+
+        do
+        {
+            Console.WriteLine("Ingrese su nombre: \n");
+            nombre = Console.ReadLine().Trim();
+
+            Console.WriteLine("Ingrese su apellido: \n");
+            apellido = Console.ReadLine().Trim();
+
+            Console.WriteLine($"Hola, {nombre} {apellido}!");
+
+            Console.WriteLine("Desea Ingresar otro nombre y apellido? (s/n)");
+            string repuesta = Console.ReadLine().ToLower();
+
+            continuar = (repuesta == "s");
+        } while( continuar );
+
+        Console.WriteLine("Gracias por utilizar este programa. Presione cualquier tecla para cerrar");
+        Console.ReadKey();
+
+        
+    }
+}
